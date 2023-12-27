921262a69b47f25e18ff630302efa837aff50e45
IsabelEstevez <147929379+IsabelEstevez@users.noreply.github.com>
2023-12-27 17:29:34 +0000

    Cambios Isa

--- src/app.py
+++ src/app.py
@@ -7,8 +7,52 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # 1) Connect to the database here using the SQLAlchemy's create_engine function
+global engine # Esto nos permite usar una variable global llamada motor
+# Una "cadena de conexión" es básicamente una cadena que contiene todas las credenciales de la base de datos juntas
+connection_string = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}?autocommit=true"
+print("Starting the connection...")
+engine = create_engine(connection_string)
+engine.connect()
+   
 
 # 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
+
+engine.execute("""
+               
+CREATE TABLE publishers(
+    publisher_id INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY(publisher_id)
+);
+
+CREATE TABLE authors(
+    author_id INT NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    middle_name VARCHAR(50) NULL,
+    last_name VARCHAR(100) NULL,
+    PRIMARY KEY(author_id)
+);
+                     
+CREATE TABLE books(
+    book_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    total_pages INT NULL,
+    rating DECIMAL(4, 2) NULL,
+    isbn VARCHAR(13) NULL,
+    published_date DATE,
+    publisher_id INT NULL,
+    PRIMARY KEY(book_id),
+    CONSTRAINT fk_publisher FOREIGN KEY(publisher_id) REFERENCES publishers(publisher_id)
+);
+
+CREATE TABLE book_authors (
+    book_id INT NOT NULL,
+    author_id INT NOT NULL,
+    PRIMARY KEY(book_id, author_id),
+    CONSTRAINT fk_book FOREIGN KEY(book_id) REFERENCES books(book_id) ON DELETE CASCADE,
+    CONSTRAINT fk_author FOREIGN KEY(author_id) REFERENCES authors(author_id) ON DELETE CASCADE
+);""")
+
 
 # 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
 
